7f33e3bae7117395293b5d67a6d46690c7877ff6
Gustav Sundin <sundin666@gmail.com>
2022-02-01 14:21:04 +0100

    Refactor

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>The Fantastic Art of G.G. Sundin</h1>
-    <p>All illustrations made by G.G. Sundin 2021-2022</p>
+    <p>All illustrations made by G.G. Sundin 2021-2022.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,94 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="The Fantastic Art of G.G. Sundin" />
+  <div class="hello">
+    <h1>The Fantastic Art of G.G. Sundin</h1>
+    <div class="imagez">
+      <Portrait image="babylon.jpg" text="Babylons Skräck" class="portrait" />
+
+      <Portrait
+        image="space_pirate.jpg"
+        text="Space Smuggler"
+        class="portrait"
+      />
+
+      <Portrait
+        image="ringed_planet.jpg"
+        text="Ringed Planet I"
+        class="portrait"
+      />
+    </div>
+
+    <div class="imagez">
+      <Portrait
+        image="neptunus.jpg"
+        text="Varlesen från Neptunus"
+        class="portrait"
+      />
+
+      <Portrait image="terras_bane.jpg" text="Terras Bane" class="portrait" />
+    </div>
+    <hr />
+    <p>© G.G. Sundin 2022</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Portrait from "@/components/Portrait.vue";
 
 @Options({
+  props: {
+    msg: String,
+  },
   components: {
-    HelloWorld,
+    Portrait,
   },
 })
-export default class Home extends Vue {}
+export default class HelloWorld extends Vue {
+  msg!: string;
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+img {
+  // max-width: 400px;
+  height: auto;
+}
+
+.hello {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.img-container {
+  // max-width: 400px;
+  position: relative;
+  // width: 50%;
+}
+.imagez {
+  max-width: 1200px;
+  margin-left: 30px;
+  margin-right: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.portrait {
+  margin: 30px;
+}
+
+@media screen and (max-width: 960px) {
+  .imagez {
+    flex-direction: column;
+  }
+}
+</style>
